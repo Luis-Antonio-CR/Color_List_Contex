da4ff602d71cfea16b1a503309f5d79c8358170c
Luis-Antonio Castaneda <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-10-10 12:16:05 +0200

    Change the add color form for use contex

--- src/Components/AddColorForm.js
+++ src/Components/AddColorForm.js
@@ -1,56 +1,15 @@
-import React, {useRef, useState} from "react";
+import React from "react";
 import { useInput } from "./hooks";
-
-const AddColorForm1 = ({ onNewColor= f => f }) => {
-    const txtTitle = useRef();
-    const hexColor = useRef();
-
-    const submit = e => {
-        e.preventDefault();
-        const title = txtTitle.current.value;
-        const color = hexColor.current.value;
-        onNewColor(title, color);
-        txtTitle.current.value = "";
-        hexColor.current.value = "";
-    }
-
-    return(
-        <form onSubmit={submit}>
-            <input ref={txtTitle} type="text" placeholder="color title ..." required />
-            <input ref={hexColor} type="color" required />
-            <button>Add</button>
-         </form>
-    )
-}
-
-const AddColorForm2 = ({ onNewColor= f => f }) => {
-    const [title, setTitle] = useState("");
-    const [color, setColor] = useState("#000");
-
-    const submit = e => {
-        e.preventDefault();
-        onNewColor(title, color);
-        setTitle("");
-        setColor("");
-        
-    }
-
-    return(
-        <form onSubmit={submit}>
-            <input value={title} onChange={event => setTitle(event.target.value)} type="text" placeholder="color title ..." required />
-            <input value={color} onChange={event => setColor(event.target.value)} type="color" required />
-            <button>Add</button>
-         </form>
-    )
-}
+import { useColors } from "../color-hook";
 
 const AddColorForm = ({ onNewColor= f => f }) => {
     const [titleProps, resetTitle] = useInput("");
     const [colorProps, resetColor] = useInput("#000");
+    const {addColor} = useColors();
 
     const submit = e => {
         e.preventDefault();
-        onNewColor(titleProps.value, colorProps.value);
+        addColor(titleProps.value, colorProps.value);
         resetTitle();
         resetColor();
         
