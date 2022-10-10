d6d31bf5048aa713456a00970ffaff25b42abeae
Luis-Antonio Castaneda <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-10-10 11:25:57 +0200

    Remove the part of app component that is no more need with the use of Contex

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React, { useState } from "react";
-import colorData from "./color-data.json"
 import ColorList from "./Components/ColorList"
 import AddColorForm from "./Components/AddColorForm";
-import { v4 } from "uuid";
 
-function App() {
-  const [colors, setColors] = useState(colorData);
+const App = () => {
   return (
-    <div className="App">
-      <AddColorForm onNewColor={(title,color)=> {
-        const newColors = [...colors, {id: v4(), title, color, rating: 0}];
-        setColors(newColors);
-      }}/>
-      <ColorList colors={colors}
-      onRemoveColor={
-        id => {
-          const newColors = colors.filter(color => color.id !== id);
-          setColors(newColors);
-        }
-      }
-      onRateColor={
-        (id,rating)=>{
-          const newColors = colors.map(color => color.id === id ? {...color, rating} : color);
-          setColors(newColors);
-        }
-      } />
-    </div>
-  );
+    <>
+      <AddColorForm />
+      <ColorList />
+    </>
+  )
 }
 
-export default App; 
+export default App;
